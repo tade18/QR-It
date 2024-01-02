2905538f9b0c4a2dd40df59960420d238dcffa44
tade18 <tadeaskeller@gmail.com>
2024-01-02 18:36:32 +0100

    update2

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,11 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
-let generateButton = document.getElementById("generateButton")
+let generateButton = document.getElementById("generateButton");
 function generateQR(){
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 }
 generateButton.onclick =()=>{
     generateQR();
+    imgBox.style.display = "block";
 }
